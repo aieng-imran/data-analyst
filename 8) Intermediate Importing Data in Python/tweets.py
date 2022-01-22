63f01810459708cb455e29806459cde1664bcb1d
Imran Yasin <iy63@cornell.edu>
2022-01-21 19:53:29 -0500

    course-8 of data-analyst

---
+++ 8) Intermediate Importing Data in Python/tweets.py
@@ -0,0 +1,5 @@
+# Create Streaming object and authenticate
+l = MyStreamListener()
+stream = tweepy.Stream(auth, l)
+# This line filters Twitter Streams to capture data by keywords:
+stream.filter(track=['apples', 'oranges'])
